462886ed434d33bb990fcae87782c18be29c455b
andrey-pereira02 <andreypereiradev@gmail.com>
2026-08-26 22:09:31 -0300

    Codigo para equivalencia logica e tentativa de codigo para tautologia

--- functions.py
+++ functions.py
@@ -23,3 +23,39 @@ def subformlas(formula: Formula):
         right = subformlas(formula.right)
 
         return {formula}.union(left).union(right)
+
+
+"""Codigo para ver se duas formulas são logicamente equivalentes """
+
+
+def is_logical_equivalence(formula1, formula2):
+
+    if isinstance(formula1, Atom) and isinstance(formula2, Atom):
+        return True if formula1.__eq__(formula2) else False
+
+    if isinstance(formula1, Not) and isinstance(formula2, Not):
+        return is_logical_equivalence(formula1.inner, formula2.inner)
+
+    if isinstance(formula1, (And, Implies, Or)) or isinstance(formula2, (And, Implies, Or)):
+
+        if type(formula1) != type(formula2):
+            return False
+
+        return is_logical_equivalence(formula1.left, formula2.left) and is_logical_equivalence(formula1.right, formula2.right)
+
+    return False
+
+
+"""Tentativa de codigo para ver se é uma tautologia,provavelmente esta BEM errado"""
+
+
+def is_valid(formula1: Formula):
+
+    if isinstance(formula1, Atom):
+        return True if formula1 == True else False
+
+    if isinstance(formula1, Not):
+        return is_valid(formula1.inner)
+
+    if isinstance(formula1, (And, Or, Implies)):
+        return is_valid(formula1.left) and is_valid(formula1.right)
